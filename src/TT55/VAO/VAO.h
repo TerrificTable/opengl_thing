23afdb03700b6b9074572f43de25c07b4fc08fd1
TerrificTable <omegagaming189@gmail.com>
2022-08-05 09:13:30 +0200

    the VAO class

---
+++ src/TT55/VAO/VAO.h
@@ -0,0 +1,24 @@
+#ifndef VAO_H
+#define VAO_H
+
+#include <glad/glad.h>
+#include "../VBO/VBO.h"
+
+
+class VAO {
+
+public:
+    GLuint ID;
+
+    VAO();
+
+
+    void LinkVBO(VBO vbo, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset);
+    void Bind();
+    void Unbind();
+    void Delete();
+
+};
+
+
+#endif
