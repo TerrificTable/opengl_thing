e2e1b596edb8a0aba9b9b9972b6a167cefc84d5c
TerrificTable <omegagaming189@gmail.com>
2022-08-05 09:13:00 +0200

    crerate window and contains Window_s struct (idk why i did that)

---
+++ src/TT55/window.h
@@ -0,0 +1,35 @@
+namespace TT55 {
+
+    // Window_s struct, contains some information about the window
+    struct Window_s {
+        GLFWwindow* window;
+        
+        int width;
+        int height;
+        const char* name;
+        unsigned int programID;
+
+
+        bool wireframe;
+        unsigned int wireframe_toggle_time;
+    } Window;
+
+
+    // Create GLFWwindow
+    GLFWwindow *createWindow(Window_s WINDOW) {
+
+        // Create the window
+        GLFWwindow* window = glfwCreateWindow(WINDOW.width, WINDOW.height, WINDOW.name, NULL, NULL);
+
+        // check if window was created successfully
+        if (!window) {
+            std::cout << "Failed to create GLFW window" << std::endl;
+            glfwTerminate(); // terminate glfw incase window was not created successfully
+            exit(1); // exit
+        }
+
+        return window; // return `window` object
+    }
+
+
+}
